b3d75a12026994264212069dabdba4f7e755500c
LIOU <aa78789898tw@gmail.com>
2022-06-27 00:29:16 +0800

    remove some comment

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -6,23 +6,6 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        // header("Access-Control-Allow-Origin: *");
-        // header("Access-Control-Allow-Methods: *");
-        // header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
-        // header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, OPTIONS");
-        // header("Access-Control-Allow-Headers: X-Requested-With");
-        // echo '123';
-
-        // header("Access-Control-Allow-Origin: *");
-        // header("Access-Control-Allow-Methods: *");
-        // header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
-
-        // header("Access-Control-Allow-Origin: *");
-        // header("Access-Control-Allow-Methods: *");
-        // header("Access-Control-Allow-Headers: Methods,AccountKey,x-requested-with, Content-Type, origin, authorization, accept, client-security-token, host, date, cookie, cookie2");
-        // header('Access-Control-Max-Age: 0');
-        // header('Access-Control-Max-Age: 86400');
         return 'welcome to my custom mvc framework !';
     }
 }
