7608ed5ec0349361a6fcec1b66305e21cb15b1b4
LIOU <aa78789898tw@gmail.com>
2022-06-28 17:46:48 +0800

    fix log bug

--- app/Debug/Log.php
+++ app/Debug/Log.php
@@ -4,14 +4,17 @@ namespace App\Debug;
 
 class Log
 {
-    const DEFAULTPATH = __DIR__."\..\..\storage\logs";
+    const DEFAULTPATH = __DIR__."/../../storage/logs";
 
     /**
      * write debug to project/storage/logs
      */
     public static function debug($file,$line,$data = null)
     {
-        // return self::DEFAULTPATH . '\debug.log';
-        file_put_contents(self::DEFAULTPATH . '\debug.log', $file."@".$line." : ".json_encode($data, JSON_PRETTY_PRINT)."\n\n", FILE_APPEND);
+        $response = $file."@".$line." : ".json_encode($data, JSON_PRETTY_PRINT)."\n\n";
+
+        file_put_contents(self::DEFAULTPATH . '/debug.log',$response , FILE_APPEND);
+
+        return $response;
     }
 }
